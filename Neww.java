e4633d43da252527f6d95f5e7cf4c46a3fd85720
logesh810 <logeshsharma165@gmail.com>
2022-02-03 15:12:55 +0530

    jenkins test

--- Neww.java
+++ Neww.java
@@ -6,7 +6,7 @@ for(int i=1;i<10;i++)
 System.out.println("new start new build .."+ i);
 }
 System.out.println("edit by sandy");
-System.out.println("edit by sandy");
 System.out.println("edit by logesh 02:49 PM 03-02-2022");
+System.out.println("edit by logesh 03:12 PM 03-02-2022");
 }
 }
