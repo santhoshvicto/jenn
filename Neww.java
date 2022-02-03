ba0bad456d65eef1b84baff63079e5eafdf1962e
santos-victory <santhoshvictorysoftwares@gmailcom>
2022-02-03 14:41:19 +0530

    update

--- Neww.java
+++ Neww.java
@@ -3,7 +3,7 @@ public static void main(String[] args)
 {
 for(int i=1;i<10;i++)
 {
-System.out.println("new start new build .."+ i);dd
+System.out.println("new start new build .."+ i);
 }
 System.out.println("edit by sandy");
 }
